d16def67c26d69f2281a582100aa51bee7a025b1
Alder-dev <pkeiner08@gmail.com>
2023-12-01 22:39:25 -0500

    Ajuste login

--- logeado.php
+++ logeado.php
@@ -1,3 +1,47 @@
+<?php
+
+include('config.php');
+
+$login_button = '';
+
+if(isset($_GET["code"])) {
+    $token = $google_client->fetchAccessTokenWithAuthCode($_GET["code"]);
+
+    if(!isset($token['error'])) {
+        $google_client->setAccessToken($token['access_token']);
+        $_SESSION['access_token'] = $token['access_token'];
+
+        $google_service = new Google_Service_Oauth2($google_client);
+
+        $data = $google_service->userinfo->get();
+
+        if(!empty($data['given_name'])) {
+            $_SESSION['user_first_name'] = $data['given_name'];
+        }
+
+        if(!empty($data['family_name'])) {
+            $_SESSION['user_last_name'] = $data['family_name'];
+        }
+
+        if(!empty($data['email'])) {
+            $_SESSION['user_email_address'] = $data['email'];
+        }
+
+        if(!empty($data['gender'])) {
+            $_SESSION['user_gender'] = $data['gender'];
+        }
+
+        if(!empty($data['picture'])) {
+            $_SESSION['user_image'] = $data['picture'];
+        }
+    }
+}
+
+if(!isset($_SESSION['access_token'])) {
+    $login_button = '<a href="'.$google_client->createAuthUrl().'"><i class="fa-brands fa-google"></i></a>';
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -44,19 +88,16 @@
                         </form>
                     </li>
                     <li>
-                        <a href="./login.php" class="btn btn-hover">
                         <?php
                             if($login_button == '') {
-                                echo '<div class="panel-heading">Welcome User</div><div class="panel-body">';
                                 echo '<img src="'.$_SESSION["user_image"].'" class="img-responsive img-circle img-thumbnail" />';
-                                echo '<h3><b>Name :</b> '.$_SESSION['user_first_name'].' '.$_SESSION['user_last_name'].'</h3>';
-                                echo '<h3><b>Email :</b> '.$_SESSION['user_email_address'].'</h3>';
-                                echo '<h3><a href="logout.php">Logout</h3></div>';
+                                echo '<b>Name :</b> '.$_SESSION['user_first_name'].' '.$_SESSION['user_last_name'].'>';
+                                echo 'Email :</b> '.$_SESSION['user_email_address'].'>';
+                                echo '<a href="logout.php">Cerrar Sesión></div>';
                             } else {
                                 echo '<div align="center">'.$login_button . '</div>';
                             }
                             ?>
-                        </a>
                     </li>
                    
                 </ul>
