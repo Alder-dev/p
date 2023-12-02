eb36bf61d038f095423e5325fce6004feb70e454
Alder-dev <pkeiner08@gmail.com>
2023-12-02 01:48:07 -0500

    Ajuste login

--- logeado.php
+++ logeado.php
@@ -1,3 +1,58 @@
+<?php
+include('config.php');
+include('conexion.php');
+
+// $para      = $_SESSION['user_email_address'];
+// $titulo    = 'Credenciales de inicio de sesión StreetMovies';
+// $mensaje   = 'Tu usuario es: ' . $usuario . ' y tu contraseña es: ' . $password;
+// $cabeceras = 'From: webmaster@example.com' . "\r\n" .
+//       'Reply-To: webmaster@example.com' . "\r\n" .
+//       'X-Mailer: PHP/' . phpversion();
+// mail($para, $titulo, $mensaje, $cabeceras) or die ('Error al enviar el correo');
+
+$login_button = '';
+
+if(isset($_GET["code"])) {
+    $token = $google_client->fetchAccessTokenWithAuthCode($_GET["code"]);
+
+    
+    if(!isset($token['error'])) {
+        $google_client->setAccessToken($token['access_token']);
+        $_SESSION['access_token'] = $token['access_token'];
+
+        $google_service = new Google_Service_Oauth2($google_client);
+
+        $data = $google_service->userinfo->get();
+        
+        
+        if(!empty($data['given_name'])) {
+            $_SESSION['user_first_name'] = $data['given_name'];
+        }
+
+        if(!empty($data['family_name'])) {
+            $_SESSION['user_last_name'] = $data['family_name'];
+        }
+        
+        if(!empty($data['email'])) {
+            $_SESSION['user_email_address'] = $data['email'];
+        }
+        
+        if(!empty($data['gender'])) {
+            $_SESSION['user_gender'] = $data['gender'];
+        }
+        
+        if(!empty($data['picture'])) {
+            $_SESSION['user_image'] = $data['picture'];
+        }
+    }
+}
+
+if(!isset($_SESSION['access_token'])) {
+    $login_button = '<a href="'.$google_client->createAuthUrl().'"><i class="fa-brands fa-google"></i></a>';
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -44,21 +99,20 @@
                         </form>
                     </li>
                     <li>
-                        <a href="./login.php" class="btn btn-hover">
                         <?php
-                            if($login_button == '') {
-                                echo '<div class="panel-heading">Welcome User</div><div class="panel-body">';
-                                echo '<img src="'.$_SESSION["user_image"].'" class="img-responsive img-circle img-thumbnail" />';
-                                echo '<h3><b>Name :</b> '.$_SESSION['user_first_name'].' '.$_SESSION['user_last_name'].'</h3>';
-                                echo '<h3><b>Email :</b> '.$_SESSION['user_email_address'].'</h3>';
-                                echo '<h3><a href="logout.php">Logout</h3></div>';
-                            } else {
-                                echo '<div align="center">'.$login_button . '</div>';
-                            }
-                            ?>
-                        </a>
+                        if ($login_button == '') {
+                            echo '<div style="text-align: center;">';
+                            echo '<img src="'.$_SESSION["user_image"].'" class="img-responsive img-circle img-thumbnail" />';
+                            echo '<br>';
+                            echo '<h3>'.$_SESSION['user_first_name'].' '.$_SESSION['user_last_name'].'</h3>';
+                            echo '<p>'.$_SESSION['user_email_address'].'</p>';
+                            echo '<a href="logout.php" class="btn btn-danger">Cerrar Sesión</a>';
+                            echo '</div>';
+                        } else {
+                            echo '<div align="center">'.$login_button.'</div>';
+                        }
+                        ?>
                     </li>
-                   
                 </ul>
                 <!-- MENU PARA CELULAR -->
                 <div class="hamburger-menu" id="hamburger-menu">
@@ -68,6 +122,7 @@
             </div>
         </div>
     </div>
+
     <!-- FIN NAV -->
 
     <!-- HERO SECTION -->
